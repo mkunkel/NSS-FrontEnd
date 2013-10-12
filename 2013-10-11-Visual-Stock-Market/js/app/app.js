21624b33c14be229a1d3caa697f7451e1899dc0b
Mike Kunkel <yourmom.tv@gmail.com>
2013-10-11 23:10:55 -0500

    visual stock market debugging flickr

--- 2013-10-11-Visual-Stock-Market/js/app/app.js
+++ 2013-10-11-Visual-Stock-Market/js/app/app.js
@@ -25,6 +25,31 @@ function initialize(){
 function stockAdded(stock) {
   stock = stock.val();
   db.stocks.push(stock);
+  displayStock(stock);
+}
+
+function displayStock(stock) {
+  var $col = $('<div>').addClass('stockColumn ' + stock.symbol);
+  var $stock = $('<div>').addClass('stock').text(stock.symbol);
+  $stock.append($('<div>').addClass('stockImage').c);
+  $col.append($stock);
+  $('#stocks').append($col);
+  getPicture(stock);
+}
+
+function getPicture(stock) {
+  debugger;
+  var apiKey = '9ef7bd8a36c1d713433c10bd95f60d37';
+  var perPage = 1;
+  var page = 1;
+  var query = stock.name;
+  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&text=' + query + '&per_page=' + perPage + '&page=' + page + '&format=json&jsoncallback=?';
+  $.getJSON(url, function(data) {
+    var photo = data.photos.photo[0];
+    var imgUrl = 'url(http://farm' + photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+    var selector = '.' + stock.symbol + ' stockImage';
+    $(selector).css('background-image', imgUrl);
+  });
 }
 
 function buyStock() {
@@ -33,7 +58,13 @@ function buyStock() {
   var quantity = parseInt($('#quantity').val(), 10);
   var url = 'http://dev.markitondemand.com/Api/Quote/jsonp?callback=?';
   requestQuote(symbol, function(data, textStatus, jqXHR) {
-
+    data = data.Data;
+    stock.symbol = data.Symbol;
+    stock.name = data.Name;
+    stock.price = data.LastPrice;
+    stock.quantity = quantity;
+    stock.boughtAt = data.LastPrice;
+    Δstocks.push(stock)
 
   });
 }
