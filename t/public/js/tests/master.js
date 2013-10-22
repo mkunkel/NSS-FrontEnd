1e2edf64439893857b275eac585bd8b826bcf57e
Mike Kunkel <yourmom.tv@gmail.com>
2013-10-22 09:09:42 -0500

    update template

--- t/public/js/tests/master.js
+++ t/public/js/tests/master.js
@@ -3,12 +3,23 @@
 // module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
 // function setupTest(){
-//   initialize(null, true);
+//   turnHandlersOff();
+//   turnHandlersOn();
+//   // Reset Global Variables Here
+//   // Clean Out Test Database Here
 // }
 
 // function teardownTest(){
 // }
 
 // test('<name-of-feature>', function(){
+//   expect(1);
+
+//   equal('actual-result', 'expected-result', 'description of assertion');
+//   ok('result-that-is-true-or-false', 'description of assertion');
+//   deepEqual('actual-result', 'expected-result', 'description of assertion');
+// });
+
+// asyncTest('<name-of-feature>', function(){
 //   expect(1);
 // });
